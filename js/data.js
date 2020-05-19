146e61181e684b6628c9e826b9fe9edfc20a3d59
Dovydasp <59613922+Dovydasp@users.noreply.github.com>
2020-05-19 10:55:16 +0300

    data for services

---
+++ js/data.js
@@ -0,0 +1,13 @@
+// SERVICES DATA
+
+
+const services = [
+{
+    icon: 'laptop',
+    title: 'Web Design',
+    text: "Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry's standard dummy text ever"
+},
+
+
+
+]
