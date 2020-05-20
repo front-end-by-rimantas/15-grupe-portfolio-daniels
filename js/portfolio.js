89da240fde66a533eb71f8ceb02c5203d20990de
Dovydasp <59613922+Dovydasp@users.noreply.github.com>
2020-05-20 21:00:20 +0300

    added image filtering

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,89 +4,76 @@ const portfolio = [
   {
     img: "./img/portfolio/1.jpg",
     title: "Design",
-    category: ["All", "Brand"],
+    category: ["all", "brand"],
   },
   {
     img: "./img/portfolio/2.jpg",
     title: "Design",
-    category: ["All", "Design"],
+    category: ["all", "design"],
   },
   {
     img: "./img/portfolio/3.jpg",
     title: "Design",
-    category: ["All", "Design"],
+    category: ["all", "design"],
   },
   {
     img: "./img/portfolio/4.jpg",
     title: "Design",
-    category: ["All", "Brand"],
+    category: ["all", "brand"],
   },
   {
     img: "./img/portfolio/5.jpg",
     title: "Design",
-    category: ["All", "Graphic"],
+    category: ["all", "graphic"],
   },
   {
     img: "./img/portfolio/6.jpg",
     title: "Design",
-    category: ["All", "Design"],
+    category: ["all", "design"],
   },
 ];
 
 
-const filterbuttons=document.querySelector('#filtering'),
-  filterbtns=filterbuttons.children,
-  totalFilterBtn=filterbtns.length,
-  galleryItems=document.querySelector(".gallery-items").children,
-  totalItems=galleryItems.length;
-  console.log(galleryItems)
+const filterbuttons=document.querySelectorAll('.filter-buttons span');
 
-  for(let i=0; i<totalFilterBtn; i++){
-    filterbtns[i].addEventListener('click', function(){
-      filterbuttons.querySelector('.active').classList.remove("active")
-      this.classList.add("active")
 
-    })
-  }
+  filterbuttons.forEach(btn => {
+    btn.addEventListener('click', function() {
+    document.querySelector('.active').classList.remove("active")
+    btn.classList.toggle("active");
+
+  const category = btn.dataset.category
+  console.log(category)
+  galleryImages(category)
+      
+  } )
+  })
   
-
-// const filterItems = document.querySelector('.gallery-img')
-
-
-
-// for (let i=0; i<filterbuttons.length; i++){
-//  filterbuttons[i].addEventListener("click", function(){
-//   for(let j=0; j<filterbuttons.length; j++){
-//     filterItems[j].classList.remove("active")
-//   }
-//   this.classList.add("active")
-//  })
-// }
-
 function galleryImages(filter) {
   let HTML = "";
-  document.querySelector(".gallery").innerHTML = HTML;
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = HTML;
 
-  for (let i = 0; i < portfolio.length; i++) {
-    if (portfolio[i].category.includes(filter)) {
-      HTML = `
-            <div>
-                <img src="${portfolio[i].img}" alt="images" data-category="${portfolio[i].category}" class="img">
-                    <div class="gallery-item">
-                        <div>
-                        <span> ${portfolio[i].title} <span>
-                        <i class="chain fa fa-chain-broken" ></i>
-                        <i class="zoom-in fa fa-search-plus" ></i>
-                        </div>
-                    </div>
-            </div>`;
-      document.querySelector(".gallery").innerHTML += HTML;
-    }
-  }
+
+  portfolio.forEach(image => {
+   if (image.category.includes(filter)){
+    HTML = `
+    <div>
+        <img src="${image.img}" alt="images" data-category="${image.category}" class="img">
+            <div class="gallery-item">
+                <div>
+                <span> ${image.title} <span>
+                <i class="chain fa fa-chain-broken" ></i>
+                <i class="zoom-in fa fa-search-plus" ></i>
+                </div>
+            </div>
+    </div>`;
+    gallery.innerHTML += HTML; 
+   }
+  })
 }
 
 
-
-galleryImages("All");
+galleryImages("all");
 
 
