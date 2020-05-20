b214bc700d90aa9910e592109588c01004c8ce8d
Edgariukstis <edgaras.banaitis@gmail.com>
2020-05-20 18:07:03 +0300

    before improvment

--- js/runningNumbers.js
+++ js/runningNumbers.js
@@ -1,57 +1,21 @@
 "use strict"
+let places = document.querySelectorAll('.counter');
+for (let i = 0; i < places.length; i++) {
+    animateValue(places[i], 3000);
+}
 
-for (let i = 0; i < 4; i++) {
-    var place = document.getElementById("value"+i);
-    var value = place.getAttribute('value');
-    const start = 0;
-    console.log(place);
-    console.log(value);
-}   
-  
-/*
-    <div id="number">5</div>
- 
-    $(document).ready(function() {
-        var target = 50;
-        var number = $('#number').text();
-    
-        while(number <= target) {
-            setTimeout(function() {
-                $('#number').text(++number);
-            }, 30);
+function animateValue(element, duration) {   
+    const end = parseInt(element.innerText);
+    let current = 0;
+    const stepTime = Math.abs(Math.floor(duration / end)); 
+    const timer = setInterval(function() {
+        current++;
+        element.innerHTML = current;
+        if (current === end) {
+            clearInterval(timer);
         }
-    });
-*/
-// var interval = setInterval(function() {
-//     $('#number').text(number);
-//     if (number >= value) clearInterval(interval);
-//     number++;
-// }, 30);
-
-
-
-
-
-// function animateValue(i) {
-//     var obj = document.getElementById('value'+i);  
-//     var end = obj.getAttribute('value');
-//     var stepTime = 300 / end;
-   
-//     var timer = setInterval(function() {
-//         obj.innerHTML =  parseInt(obj.innerHTML)+1;
-//         if ( obj.innerHTML == end) {
-//             clearInterval(timer);
-//         }
-//     }, stepTime);  
-// }
-// function callAnimateValue(){
-      
-//     for (let i = 0; i < 4; i++) {
-//         animateValue(i);
-//     }
-// }
-// callAnimateValue();
-
+    }, stepTime);
+}
 
 
 
